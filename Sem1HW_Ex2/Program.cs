1c69c09311d3775820a5fbf41d3b89a1dcd4aee9
Robopin <vlasizov@yandex.ru>
2022-11-24 16:37:15 +0300

    Сделал 2 задачи

---
+++ Sem1HW_Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine($"Максимальное число: {numA}, минимальное число: {numB}.");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {numB}, минимальное число: {numA}.");
+}
---
+++ Sem1HW_Ex4/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+Console.WriteLine("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int numC = Convert.ToInt32(Console.ReadLine());
+*/
