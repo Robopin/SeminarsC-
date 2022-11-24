c25b8b441fa7ceadff41cd3774040244e4eca789
Robopin <vlasizov@yandex.ru>
2022-11-24 19:03:44 +0300

    4

--- Sem1HW_Ex4/Program.cs
+++ Sem1HW_Ex4/Program.cs
@@ -1,8 +1,16 @@
-﻿/*
-Console.WriteLine("Введите число A");
+﻿Console.WriteLine("Введите число A");
 int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B");
 int numB = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число C");
 int numC = Convert.ToInt32(Console.ReadLine());
-*/
+int result = numA;
+if (numB > numA)
+{
+    result = numB;
+}
+if (numC > numB)
+{
+    result = numC;
+}
+Console.WriteLine($"Максимальное число {result}");
