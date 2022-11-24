63e1aac9ca049f54a1723bad94db2db474c51dab
Robopin <vlasizov@yandex.ru>
2022-11-24 19:17:51 +0300

    6

---
+++ Sem1HW_Ex6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int remains = number % 2;
+if (remains == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
