9e64bca66901c59881a002e4a4eacec1d43b5e35
Robopin <vlasizov@yandex.ru>
2022-11-24 19:40:10 +0300

    8

--- Sem1HW_Ex8/Program.cs
+++ Sem1HW_Ex8/Program.cs
@@ -1,5 +1,6 @@
 ﻿int number = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; number < length; i+=2)
+for (int i = 1; i <= number; i++)
 {
-    Console.Write($"{number},");
+    if (i % 2 == 0)
+    Console.Write($"{i},");
 }
