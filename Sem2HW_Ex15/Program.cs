05ea538731e14cd8b6629cc940e9c820c7f382c0
Robopin <vlasizov@yandex.ru>
2022-11-25 17:23:06 +0300

    Add Ex15

--- Sem2HW_Ex15/Program.cs
+++ Sem2HW_Ex15/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number > 0 && number < 7)
+if (number > 0 && number <= 7)
 {
     if (number >= 6) Console.WriteLine("Да");
     else Console.WriteLine("Нет");
