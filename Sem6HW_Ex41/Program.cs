8eca5c8371bde3af6a36129e89833983d9943b63
Robopin <vlasizov@yandex.ru>
2022-12-14 17:15:15 +0300

    add 41, 43, 45

---
+++ Sem6HW_Ex41/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] GetMassive(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i} число массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+int Counter(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] arr = GetMassive(5);
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine(Counter(arr));
---
+++ Sem6HW_Ex45/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] GetArray(int len)
+{
+    int [] arr = new int [len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(-10, 11);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] sArray = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        sArray[i] = array[i];
+    }
+    return sArray;
+}
+int[] firstArr = GetArray(5);
+PrintArray(firstArr);
+Console.WriteLine();
+int[] secArray = CopyArray(firstArr);
+PrintArray(secArray);
