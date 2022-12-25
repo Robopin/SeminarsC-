c360b2ececcddbbfbaaf431463e1fbed8022f419
Robopin <vlasizov@yandex.ru>
2022-12-25 15:41:33 +0300

    add Ex64,66,68

---
+++ Sem9HW_Ex66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int m, int n)
+{
+    if (m == 0) return (n * (n + 1)) / 2;
+    else if (n == 0) return (m * (m + 1)) / 2;
+    else if (m == n) return m;
+    else if (m < n) return n + SumNumbers(m, n - 1);
+    else return n + SumNumbers(m, n + 1);
+}
+Console.WriteLine($"{SumNumbers(4, 8)}");
---
+++ Sem9HW_Ex68/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int FunAkk(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return FunAkk(n - 1, 1);
+    else
+      return FunAkk(n - 1, FunAkk(n, m - 1));
+}
+Console.WriteLine(FunAkk(2,3));
